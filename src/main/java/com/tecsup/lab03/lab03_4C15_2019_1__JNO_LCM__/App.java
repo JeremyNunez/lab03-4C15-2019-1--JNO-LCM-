c0e82333e1efa6b9b18ef8e23a591458e967d4db
Jeremy <jeremi.nunez@tecsup.edu.pe>
2019-03-14 09:55:28 -0500

    first commit

---
+++ src/main/java/com/tecsup/lab03/lab03_4C15_2019_1__JNO_LCM__/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab03.lab03_4C15_2019_1__JNO_LCM__;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
